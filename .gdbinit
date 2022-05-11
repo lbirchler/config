053fa1c5cbdf4c71b1b49187fe32504d2d31cdfe
lbirchler <bplyr@tutanota.com>
2022-05-11 02:18:39 -0400

    Updated ipython prompt

--- .gdbinit
+++ .gdbinit
@@ -5,7 +5,7 @@ set $base = 0x555555554000
 #set disable-randomization on
 
 # pwndbg
-source ~/tools/pwndbg/gdbinit.py
+source /opt/pwndbg/pwndbg/gdbinit.py
 set context-code-lines 15
 set show-compact-regs on
 
--- .ipython/ipython_config.py
+++ .ipython/ipython_config.py
@@ -1,1 +1,14 @@
+from IPython.terminal.prompts import Prompts, Token
+ 
+class Prompt(Prompts):
+    def in_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, '>>> '), ]
+    def continuation_prompt_tokens(self, cli=None, width=None):
+        return [(Token.Prompt, '    '), ]
+    def out_prompt_tokens(self, cli=None):
+        return [(Token.Prompt, ''), ]
+
+c.TerminalInteractiveShell.prompts_class = Prompt
+c.TerminalInteractiveShell.confirm_exit = False
+c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.editing_mode = 'vi'
