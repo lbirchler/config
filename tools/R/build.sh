2845d7567cc312938c8dcd94b814c52d6f485344
lbirchler <bplyr@tutanota.com>
2022-05-26 06:45:31 -0400

    Removed strace

--- tools/R/build.sh
+++ tools/R/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 cp ../../{.vimrc,.bashrc,.inputrc,.tmux.conf} .
 docker build --platform=linux/amd64 -t lbirchler/r-dev .
-rm {.vimrc,.bashrc,.inputrc,.tmux.conf}
+rm .vimrc .bashrc .inputrc .tmux.conf
