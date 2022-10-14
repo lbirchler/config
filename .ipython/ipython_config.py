dce49f541556a8024e1cc0e91857d23c495d71bb
lbirchler <bplyr@tutanota.com>
2022-10-14 15:44:19 -0400

    Added autoreload extension

--- .ipython/ipython_config.py
+++ .ipython/ipython_config.py
@@ -8,8 +8,15 @@ class Prompt(Prompts):
     def out_prompt_tokens(self, cli=None):
         return [(Token.Prompt, ''), ]
 
+#c.TerminalInteractiveShell.highlighting_style = 'default' 
 c.TerminalInteractiveShell.prompts_class = Prompt
-c.TerminalInteractiveShell.highlighting_style = 'material' 
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShellApp.extensions = [
+    'autoreload',
+]
+c.InteractiveShellApp.exec_lines = [
+    '%autoreload 2'
+]
+
