b7fcc39ec5cb6efb9a433996e325e91680dabdf0
lbirchler <bplyr@tutanota.com>
2022-11-10 23:59:41 -0500

    conda dev env

--- tools/conda/run.sh
+++ tools/conda/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -x
 docker run --rm -it \
+    -p 8888:8888 \
     --hostname conda --name conda \
     --platform=linux/amd64 \
+    -v $(pwd):/root/work \
     lbirchler/conda-dev /bin/bash
+    
+# jupyter lab --ip=0.0.0.0 --no-browser --allow-root
