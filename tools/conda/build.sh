ad10ff9b3a690ce176456d05e7eb290a6988f8b3
lbirchler <bplyr@tutanota.com>
2023-01-30 10:36:59 +0000

    Deleted conda

--- tools/conda/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -x
-cp -r ../../{.vimrc,.bashrc,.inputrc,.tmux.conf,.ipython} .
-DOCKER_BUILDKIT=1 docker build --platform=linux/amd64 -t lbirchler/conda-dev .
-rm -rf .vimrc .bashrc .inputrc .tmux.conf .ipython
