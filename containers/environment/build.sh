3a3d1e407b47f5aee5cf6c3c64c9646906e0eb0f
lbirchler <bplyr@tutanota.com>
2023-04-15 12:53:27 +0000

    removed ipython install.sh

--- containers/environment/build.sh
+++ containers/environment/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ../../{.vimrc,.bashrc,.inputrc,.tmux.conf,.gdbinit,ipython/*} .
+cp ../../{.vimrc,.bashrc,.inputrc,.tmux.conf,.gdbinit,ipython/*py} .
 docker build --platform=linux/amd64 -t lbirchler/env .
 rm .vimrc .bashrc .inputrc .tmux.conf .gdbinit *.py 
